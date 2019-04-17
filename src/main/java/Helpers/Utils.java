62c740a5f93b79a3804ab050bb31dc316d526b4d
Hung-Pham-Quoine <hung.pham@quoine.com>
2019-04-17 14:10:17 +0700

    Add OTP generator

---
+++ src/main/java/Helpers/Utils.java
@@ -0,0 +1,19 @@
+package Helpers;
+
+import org.jboss.aerogear.security.otp.Totp;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Utils {
+    public static String getOTP(String code) {
+        //Properties OTP;
+
+        //OTP = PropertiesLoader.load_OTP_Properties();
+
+        //String otpKeyStr = OTP.getProperty(kindOfOTP); // <- this 2FA secret key.
+
+        Totp totp = new Totp(code);
+        return totp.now();
+    }
+}
