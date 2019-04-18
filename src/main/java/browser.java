cae8e838188e5b154911db037dddd6612c051c00
Jason <pqdhung@gmail.com>
2019-04-18 12:05:59 +0700

    Add mobile chrome browser steps

--- src/main/java/browser.java
+++ src/main/java/browser.java
@@ -9,6 +9,8 @@ public class browser extends baseChrome {
         AndroidDriver<AndroidElement> driver = baseChrome.Capabilities("emulator");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.get("https://www.google.com");
+        driver.get("https://www.liquid.com");
+        driver.findElementByXPath("//i[@class='icon icon-menu-to-arrow-right-3']").click();
+        driver.findElementByXPath("(//a[text()='Sign In' and @class='header-auth__link' ])[2]").click();
     }
 }
