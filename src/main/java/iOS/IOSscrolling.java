4d03b9a01ad48d1785d28f8cfd64e24cf37233a6
Jason <pqdhung@gmail.com>
2019-04-22 13:27:35 +0700

    Add more scrolling feature to IOS

--- src/main/java/iOS/IOSscrolling.java
+++ src/main/java/iOS/IOSscrolling.java
@@ -20,5 +20,20 @@ public class IOSscrolling extends IOSBase {
         for(int i = 0; i < 3; i++)
             driver.findElementByXPath("(//XCUIElementTypeButton[@name='Increment'])[1]").click();
         System.out.println(driver.findElementByXPath("//XCUIElementTypeStaticText[@name=\"3\"]").getText());
+
+        //Back and go to picker view
+        driver.navigate().back();
+
+        while (!driver.findElementByAccessibilityId("Picker View").isDisplayed()) {
+            JavascriptExecutor js = driver;
+            HashMap<String, String> scrollObject = new HashMap<>();
+            scrollObject.put("direction", "down");
+            js.executeScript("mobile: swipe", scrollObject);
+        }
+
+        driver.findElementByAccessibilityId("Picker View").click();
+        driver.findElementByName("Green color component value").sendKeys("220");
+        driver.findElementsByClassName("XCUIElementTypePickerWheel").get(0).sendKeys("55");
+        driver.findElementByXPath("//XCUIElementTypePickerWheel[@name='Blue color component value']").sendKeys("0");
     }
 }
