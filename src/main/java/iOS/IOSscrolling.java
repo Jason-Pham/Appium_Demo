09c26c25dc9aa74e4b9c8690e6ef1d7aa4088f38
Jason <pqdhung@gmail.com>
2019-04-23 15:57:25 +0700

    Update scrolling function

--- src/main/java/iOS/IOSscrolling.java
+++ src/main/java/iOS/IOSscrolling.java
@@ -9,15 +9,14 @@ public class IOSscrolling extends IOSBase {
     public static void main(String[] args) throws MalformedURLException {
         driver = capabilities();
 
-        while (!driver.findElementByAccessibilityId("Switches").isDisplayed()) {
-            JavascriptExecutor js = driver;
-            HashMap<String, String> scrollObject = new HashMap<>();
-            scrollObject.put("direction", "up");
-            js.executeScript("mobile: swipe", scrollObject);
-        }
+        JavascriptExecutor js = driver;
+        HashMap<String, String> scrollObject = new HashMap<>();
+        scrollObject.put("direction", "down");
+        scrollObject.put("element", driver.findElementByAccessibilityId("Switches").getId());
+        js.executeScript("mobile: scroll", scrollObject);
 
         driver.findElementByAccessibilityId("Steppers").click();
-        for(int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
             driver.findElementByXPath("(//XCUIElementTypeButton[@name='Increment'])[1]").click();
         System.out.println(driver.findElementByXPath("//XCUIElementTypeStaticText[@name=\"3\"]").getText());
 
@@ -25,8 +24,8 @@ public class IOSscrolling extends IOSBase {
         driver.navigate().back();
 
         while (!driver.findElementByAccessibilityId("Picker View").isDisplayed()) {
-            JavascriptExecutor js = driver;
-            HashMap<String, String> scrollObject = new HashMap<>();
+            //JavascriptExecutor js = driver;
+            //HashMap<String, String> scrollObject = new HashMap<>();
             scrollObject.put("direction", "down");
             js.executeScript("mobile: swipe", scrollObject);
         }
